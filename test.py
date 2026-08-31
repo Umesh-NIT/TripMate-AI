0bb570831d85cf7b2efd5707be3a912089b5ec7a
Umesh-NIT <192072544+Umesh-NIT@users.noreply.github.com>
2026-08-31 12:50:40 +0530

    dockerFile Added and write frontend and backend code

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from tools import tavily_tool
 from tools import flight_tool
-
+from backend import run_travel_agent
 #res = tavily_tool.tavily_search("best hotels in indonesia")
-res = flight_tool.search_flights("Plan a 7 days Japan trip from Bangladesh")
+#res = flight_tool.search_flights("Plan a 7 days Japan trip from Bangladesh")
+#print(res)
+res = run_travel_agent("Plan a 7 days Japan trip from Bangladesh")
 print(res)
